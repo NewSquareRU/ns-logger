2ba0a7369c809cff0ff8553de923cd03b69e40cc
dpetrov <dpetrov@mabius.ru>
2016-04-12 13:04:39 +0300

    Change fast logger to logging-effect

--- src/NSLogger.hs
+++ src/NSLogger.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module NSLogger
@@ -5,74 +6,86 @@ module NSLogger
        where
 
 
-import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.ByteString.Lazy (ByteString)
+import Control.Monad.IO.Class (MonadIO)
 import Data.Time
-       (UTCTime, getCurrentTime, formatTime, defaultTimeLocale)
+       (UTCTime, formatTime, defaultTimeLocale)
 import GHC.Generics (Generic)
 import Data.Aeson (ToJSON, encode)
-import System.Log.FastLogger (ToLogStr(..), LogStr)
+import Control.Monad.Log
+       (WithTimestamp(..), WithSeverity(..), Severity(..), MonadLog)
+
+import qualified Control.Monad.Log as Log
 
 
-data Severity
-    = DEFAULT
-    | DEBUG
-    | INFO
-    | NOTICE
-    | WARNING
-    | ERROR
-    | CRITICAL
-    | ALERT
-    | EMERGENCY
-    deriving (Eq,Ord,Show,Generic)
-
-instance ToJSON Severity
-
-
-data NSLogStr a = NSLogStr
-    { time :: String
-    , severity :: Severity
-    , logMessage :: a
-    } deriving (Eq,Ord,Show,Generic)
-
-
-instance ToJSON a => ToJSON (NSLogStr a)
-
-
-instance ToJSON a => ToLogStr (NSLogStr a) where
-  toLogStr = toLogStr . encode
-
-
-logNS :: ToJSON a => UTCTime -> Severity -> a -> LogStr
+logNS
+    :: (ToJSON a, MonadIO m, MonadLog (WithTimestamp (WithSeverity a)) m)
+    => UTCTime -> Severity -> a -> m ()
 logNS timestamp severity' msg =
-    toLogStr (NSLogStr (utc2unixtimestamp timestamp) severity' msg)
+    Log.logMessage
+        (WithTimestamp
+         { discardTimestamp = (WithSeverity
+                               { discardSeverity = msg
+                               , msgSeverity = severity'
+                               })
+         , msgTimestamp = timestamp
+         })
 
 
 logNSSeverity
-    :: (MonadIO m, ToJSON a)
-    => Severity -> a -> m LogStr
+    :: (ToJSON a, MonadIO m, MonadLog (WithTimestamp (WithSeverity a)) m)
+    => Severity -> a -> m ()
 logNSSeverity severity' msg =
-    liftIO getCurrentTime >>=
-    \t ->
-         return (logNS t severity' msg)
+    Log.timestamp
+        (WithSeverity
+         { discardSeverity = msg
+         , msgSeverity = severity'
+         }) >>=
+    Log.logMessage
 
 
 logNSINFO
-    :: (MonadIO m, ToJSON a)
-    => a -> m LogStr
-logNSINFO = logNSSeverity INFO
+    :: (ToJSON a, MonadIO m, MonadLog (WithTimestamp (WithSeverity a)) m)
+    => a -> m ()
+logNSINFO = logNSSeverity Informational
 
 
 logNSDEBUG
-    :: (MonadIO m, ToJSON a)
-    => a -> m LogStr
-logNSDEBUG = logNSSeverity DEBUG
+    :: (ToJSON a, MonadIO m, MonadLog (WithTimestamp (WithSeverity a)) m)
+    => a -> m ()
+logNSDEBUG = logNSSeverity Debug
 
 
 logNSERROR
-    :: (MonadIO m, ToJSON a)
-    => a -> m LogStr
-logNSERROR = logNSSeverity ERROR
+    :: (ToJSON a, MonadIO m, MonadLog (WithTimestamp (WithSeverity a)) m)
+    => a -> m ()
+logNSERROR = logNSSeverity Error
 
+data NSLogLine a = NSLogLine
+    { severity :: String
+    , time :: String
+    , logMessage :: a
+    } deriving (Generic)
+
+instance ToJSON a => ToJSON (NSLogLine a)
+
+renderNSLog :: (ToJSON a) => WithTimestamp (WithSeverity a) -> ByteString
+renderNSLog msg =
+    encode $
+    NSLogLine
+    { severity = severity2nsseverity (msgSeverity (discardTimestamp msg))
+    , time = utc2unixtimestamp (msgTimestamp msg)
+    , logMessage = discardSeverity (discardTimestamp msg)
+    }
 
 utc2unixtimestamp :: UTCTime -> String
 utc2unixtimestamp = formatTime defaultTimeLocale "%s"
+
+
+severity2nsseverity :: Severity -> String
+severity2nsseverity sevrty =
+    case sevrty of
+        Informational -> "INFO"
+        Debug -> "DEBUG"
+        Error -> "ERROR"
+        _ -> show sevrty
